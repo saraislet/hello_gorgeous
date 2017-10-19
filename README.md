0531f0268730b5badd549f9018babbf3f7606bf2
Sarai <anna.sarai.rosenberg@gmail.com>
2017-10-19 14:03:38 -0700

    add display template

--- README.md
+++ README.md
@@ -1,13 +1,17 @@
 # Hello, Gorgeous
 Twitter API to replace "Hello, world" with words of affirmation.
 
+Live: https://sheltered-savannah-11540.herokuapp.com/hello
+
 ## Using hello_gorgeous
 
 ### From the command line
-
-* curl https://sheltered-savannah-11540.herokuapp.com/
+```
+curl https://sheltered-savannah-11540.herokuapp.com/
+```
 
 ### Python
-
-* import requests
-* requests.get('https://sheltered-savannah-11540.herokuapp.com/').text
+```
+import requests
+requests.get('https://sheltered-savannah-11540.herokuapp.com/').text
+```
--- app.py
+++ app.py
@@ -6,23 +6,37 @@ Created on Wed Oct 18 13:25:20 2017
 """
 
 from random import randint
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
+quotes = [ "Hello, gorgeous",
+           "You look good today",
+           "You are valued, and you are worthy",
+           "You do good work",
+           "You can do the thing",
+           "You inspire people",
+           "You rock",
+           "You're appreciated"  ]
 
 
 @app.route("/")
 def main():
     # Return words of affirmation
-    quotes = [ "Hello, gorgeous",
-               "You look good today",
-               "You are valued, and you are worthy",
-               "You're appreciated"  ]
     randomNumber = randint(0,len(quotes)-1) 
     quote = quotes[randomNumber]
     
     return quote
 
+
+@app.route("/hello")
+def hello():
+    # Return words of affirmation
+    randomNumber = randint(0,len(quotes)-1) 
+    quote = quotes[randomNumber]
+    
+    return render_template('hello.html', quote=quote)
+
+
 if __name__ == "__main__":
-#    app.run(host='0.0.0.0', port=1313)
     app.run()
